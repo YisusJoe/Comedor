39b0aed6ee129bdc71fe91da171706107eea27f1
YisusJoe <yisusjoe.mx@gmail.com>
2023-04-15 00:38:22 -0700

    Creacion estetica login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FullLayoutComponent } from './full-layout/full-layout.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FullLayoutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
